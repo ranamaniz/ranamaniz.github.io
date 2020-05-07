85c972b26fadf16959aa80619a66c7790b2aa207
Manish Rana <alertmanishrana@gmail.com>
2020-05-07 21:14:42 +0545

    Updates

---
+++ precache-manifest.40ff315e688fdfaf5ba4b466adf2b9c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e523dc36bf1fac496864acc5334184ac",
+    "url": "/ranamaniz.github.io/index.html"
+  },
+  {
+    "revision": "431ae9e9427ebea2862c",
+    "url": "/ranamaniz.github.io/static/css/main.b9ee4da1.chunk.css"
+  },
+  {
+    "revision": "09b0ac98dfe2d6cf96fc",
+    "url": "/ranamaniz.github.io/static/js/2.5761c3f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ranamaniz.github.io/static/js/2.5761c3f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "431ae9e9427ebea2862c",
+    "url": "/ranamaniz.github.io/static/js/main.2ba37522.chunk.js"
+  },
+  {
+    "revision": "08817272c794610aad1a",
+    "url": "/ranamaniz.github.io/static/js/runtime-main.f68215b7.js"
+  },
+  {
+    "revision": "c0ba5194027e484e799e858de891a483",
+    "url": "/ranamaniz.github.io/static/media/myimage.c0ba5194.jpg"
+  }
+]);
