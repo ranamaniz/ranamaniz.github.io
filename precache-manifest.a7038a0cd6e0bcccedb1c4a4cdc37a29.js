3191f22cf914f763855044ce0c3077f14267bf5f
Manish Rana <alertmanishrana@gmail.com>
2020-05-07 21:21:26 +0545

    Updates

---
+++ precache-manifest.a7038a0cd6e0bcccedb1c4a4cdc37a29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf9068b459e4887ef724140b2ab54f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9825162c8b7d6c3fc15",
+    "url": "/static/css/main.b9ee4da1.chunk.css"
+  },
+  {
+    "revision": "09b0ac98dfe2d6cf96fc",
+    "url": "/static/js/2.5761c3f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5761c3f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9825162c8b7d6c3fc15",
+    "url": "/static/js/main.4d3ce7c4.chunk.js"
+  },
+  {
+    "revision": "ad48f1be43dde9a59d6f",
+    "url": "/static/js/runtime-main.b350293a.js"
+  },
+  {
+    "revision": "c0ba5194027e484e799e858de891a483",
+    "url": "/static/media/myimage.c0ba5194.jpg"
+  }
+]);
